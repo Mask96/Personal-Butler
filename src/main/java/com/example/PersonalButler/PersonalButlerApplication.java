b3744bdbb92dfc2ae7b8c42bfc0a73af4bb3c242
Mask96 <781064841@qq.com>
2020-09-16 15:50:37 +0800

    创建

---
+++ src/main/java/com/example/PersonalButler/PersonalButlerApplication.java
@@ -0,0 +1,13 @@
+package com.example.PersonalButler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalButlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalButlerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/PersonalButler/PersonalButlerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PersonalButler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalButlerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
